48c1542734356c254a14a26a35466ca498fd1e7b
jocelyn <jocelyn@amtsolutions.fr>
2019-10-08 11:21:11 +0200

    fix block

--- test.js
+++ test.js
@@ -10,8 +10,13 @@ logger.warning('Warning message');
 logger.error('Error message');
 logger.success('Success message');
 
+console.log('');
 logger.debug('Debug message', { block: true });
+console.log('');
 logger.info('Info message', { block: true });
+console.log('');
 logger.warning('Warning message', { block: true });
+console.log('');
 logger.error('Error message', { block: true });
+console.log('');
 logger.success('Success message', { block: true });
