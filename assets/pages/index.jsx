7d6bf528ac00ff7549b49b54226aab71798d65a0
Jocelyn <jocelyn.faihy@gmail.com>
2019-12-18 13:00:28 +0100

    add color full support

---
+++ assets/pages/index.jsx
@@ -0,0 +1,30 @@
+
+const logger = require('../../lib');
+
+logger.log('Hello world');
+
+logger.debug('Debug message');
+logger.info('Info message');
+logger.success('Success message');
+logger.warning('Warning message');
+logger.error('Error message');
+logger.error(new Error('Message\nwith comment'));
+
+logger.debug('Debug block', { block: true });
+logger.info('Info block', { block: true });
+logger.success('Success block', { block: true });
+logger.warning('Warning block', { block: true });
+logger.error('Error block', { block: true });
+
+logger.table([
+    ['Nom', 'Valeur'],
+    ['x', 1],
+    ['loonnnnng first value', 2],
+    ['c', 'loooooonnnnng second value'],
+    [3, 'a'],
+]);
+
+logger.level = 1;
+logger.debug('Debug message');
+logger.info('Info message');
+logger.success('Success message');
